7a17ae17e1457635a2a5448722e4b31d0e14f570
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-08-16 12:45:19 +1200

    Updated lighting and started code for heart gui

--- Sprite2D.gd
+++ Sprite2D.gd
@@ -1,5 +1,6 @@
 extends Sprite2D
-
+var health = 0
+var start_position3 = Vector3(10 ,10)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,6 +11,8 @@ func _ready():
 func _process(delta):
 	pass
 
-
+#func _on_enemy_player_dies():
+	#self.visible = false
+	
 func _on_enemy_player_dies():
-	self.visible = false
+	self.position = start_position3
