e99939257cf515529e015022739b1cb2eeeb4958
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-09-23 10:50:58 +1200

    Rocket fuel animations

--- Global.gd
+++ Global.gd
@@ -5,3 +5,5 @@ var rocket_fueled: bool = false
 var player_visible = 1
 var chase: bool = false
 var allowed_to_move: bool = true
+var rocket_fully_fueled: bool = false
+var fuel_animation: bool = false
--- Rocket.gd
+++ Rocket.gd
@@ -6,14 +6,17 @@ var crystal_count = 0
 var speed: float = -10.0
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	label.visible = false
+	label.visible = true
 	$CPUParticles2D.emitting = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-		if Input.is_action_just_pressed("fueling rocket") and Global.green_obtained and label.visible:
+		if Input.is_action_just_pressed("fueling rocket") and Global.green_obtained and Global.red_obtained and label.visible:
 			print("animationed")
-			fuel_container.play("default")
+			Global.fuel_animation == true
+			Global.rocket_fully_fueled = true
+		
+		fuel_container.visible == true
 		
 		if Global.rocket_fueled == true:
 			Global.player_visible = 2
@@ -22,13 +25,19 @@ func _process(delta):
 		if Global.allowed_to_move == false:
 			self.position.y += speed
 		
-		if Global.green_obtained and Global.red_obtained:
-			await get_tree().create_timer(2).timeout
+		if Global.rocket_fully_fueled == true:
+			await get_tree().create_timer(4).timeout
 			Global.rocket_fueled = true
 			label.visible = false
+			Global.fuel_animation == false
 			
 		if Global.rocket_fueled == true:
 			$CPUParticles2D.emitting = true
+		
+		if Global.fuel_animation == true:
+			fuel_container.play("new_animation")
+		elif Global.fuel_animation == false:
+			Global.fuel_animation == false
 
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("player"):
